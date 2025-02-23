ef4e98b78cd65f617ff1fdf53e058daeb8886443
Kyptin <144300718+MeredithDelatorre@users.noreply.github.com>
2025-02-23 15:01:03 -0500

    hover state

---
+++ VR World/Assets/Bow and Arrow Project 2025/Scripts/GumballPopupHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class GumballPopupHandler : MonoBehaviour {
+    public GameObject popup;
+
+    private void Start() {
+        popup.SetActive(false); 
+    }
+
+    private void OnEnable() {
+        XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
+        interactable.hoverEntered.AddListener(OnHoverEnter);
+        interactable.hoverExited.AddListener(OnHoverExit);
+    }
+
+    private void OnDisable() {
+        XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
+        interactable.hoverEntered.RemoveListener(OnHoverEnter);
+        interactable.hoverExited.RemoveListener(OnHoverExit);
+    }
+
+    private void OnHoverEnter(HoverEnterEventArgs args) {
+        popup.SetActive(true);
+    }
+
+    private void OnHoverExit(HoverExitEventArgs args) {
+        popup.SetActive(false);
+    }
+
+}
